891e4bd28436ef18732355116d2e443a92d1cd11
elinaail <vasavasorum1@gmail.com>
2022-06-28 23:15:44 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+EtoF = {'bread': 'du pain', 'wine': 'du vin',\
+'eats': 'mange', 'drinks': 'bois',\
+'likes': 'aime', 1: 'un',\
+'6.00':'6.00'}
+def translateWord(word, dictionary):
+  if word in dictionary:
+     return dictionary[word]
+  else:
+     return word
+def translate(sentence):
+  translation = ''
+  word = ''
+  for c in sentence:
+    if c != ' ':
+       word = word + c
+    else:
+      translation = translation + ' ' + translateWord(word, EtoF)
+      word = ''
+  return translation[1:] + ' ' + translateWord(word, EtoF)
+
+print (translate('John eats bread'))
+print (translate('Eric drinks wine'))
+print (translate('Everyone likes 6.00'))
